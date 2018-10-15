d3ac815db75111cfe87a42da5b217c44c8dabebc
Niteshkumar <nitesh156200@gmail.com>
2018-10-15 15:41:58 -0400

    Validation almost done
    Need to connect with oracle

--- mail.php
+++ mail.php
@@ -1,31 +1,40 @@
-<?php
-    $to = 'demo@site.com';
-    $name = $_POST["name"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $subject= $_POST["subject"];
-    
+<?php 
 
+      $password = trim($_POST['password']);
+      $confirm_password = trim($_POST['confirm_password']);      
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$name.'  '.$subject.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$subject.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-        
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'Your message has been sent.';
-    }else{
-        echo 'failed';
-    }
+      if(empty(trim($_POST['password']))){
+          $password_err = "Please enter a password.";     
+      } elseif(strlen(trim($_POST['password'])) < 6){
+          $password_err = "Password must have atleast 6 characters.";
+      } else{
+          $password = trim($_POST['password']);
+      }
+      // Validate confirm password
+      if(empty(trim($_POST["confirm_password"]))){
+          $confirm_password_err = 'Please confirm password.';     
+      } else{
+          $confirm_password = trim($_POST['confirm_password']);
+          if($password != $confirm_password){
+              $confirm_password_err = 'Password did not match.';
+          }
+      }
+      // Validate email
+      if(empty(trim($_POST['email']))){
+          $email_err = "Please enter an email.";     
+      } elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+          $email_err = "Invalid email format";
+      } else{
+          $email = trim($_POST['email']);
+      }
+      // Validate confirm password
+      if(empty(trim($_POST["confirm_password"]))){
+          $confirm_password_err = 'Please confirm password.';     
+      } else{
+          $confirm_password = trim($_POST['confirm_password']);
+          if($password != $confirm_password){
+              $confirm_password_err = 'Password did not match.';
+          }
+      }
 
 ?>
